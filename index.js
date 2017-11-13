e7a05fdfba9182328b6dda5064f5cc39c055ddea
Sean Elwood <seanelwood@gmail.com>
2017-11-13 01:04:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Code your solution in this file.
 function lowerCaseDrivers(drivers) {
-  drivers.map( function (driver) { return driver.toLowerCase(x)})
+  drivers.map( function (driver) { return driver.toLowerCase()})
 }
